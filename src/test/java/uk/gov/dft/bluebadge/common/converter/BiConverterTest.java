2d9e0f0115b63909f46f39080682a66131106b6d
Rob Smallwood <rob.smallwood@culturatech.com>
2018-07-06 11:33:55 +0100

    Added BiConverter test

---
+++ src/test/java/uk/gov/dft/bluebadge/common/converter/BiConverterTest.java
@@ -0,0 +1,39 @@
+package uk.gov.dft.bluebadge.common.converter;
+
+import org.junit.Test;
+import uk.gov.dft.bluebadge.common.service.exception.BadRequestException;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class BiConverterTest {
+
+  @Test
+  public void convertToModelList() {
+
+    // Given
+    BiConverter<String, String> testConverter = new BiConverter<String, String>() {
+      @Override
+      public String convertToEntity(String model) throws BadRequestException {
+        return null;
+      }
+
+      @Override
+      public String convertToModel(String entity) {
+        return entity;
+      }
+    };
+
+    String[] values = {"Fred", "James", "Dotty"};
+    List<String> strings = Arrays.asList(values);
+
+    // When
+    List<String> result = testConverter.convertToModelList(strings);
+
+    assertThat(result).contains(values);
+
+  }
+}
