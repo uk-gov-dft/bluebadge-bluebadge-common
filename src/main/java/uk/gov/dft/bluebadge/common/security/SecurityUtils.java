b412d582d2435589c32216c24d1f6415248dc21b
Robert Smallwood <rob.smallwood@valtech.co.uk>
2018-07-23 13:00:14 +0100

    Added SecurityUtils to common code

---
+++ src/main/java/uk/gov/dft/bluebadge/common/security/SecurityUtils.java
@@ -0,0 +1,60 @@
+package uk.gov.dft.bluebadge.common.security;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import uk.gov.dft.bluebadge.common.security.model.LocalAuthority;
+import uk.gov.dft.bluebadge.common.security.model.User;
+
+@Component
+public class SecurityUtils {
+  private static final int MOCK_LOCAL_AUTHORITY = 22;
+  private static final Integer MOCK_ROLE_ID = 1;
+
+  /**
+   * Returns the currently logged in user.
+   * @return
+   */
+  public User getCurrentUserDetails() {
+
+    Authentication authentication = getCurrentAuthenticationContext();
+
+    return User.builder()
+      .localAuthority(createMockLocalAuthority())
+      .emailAddress(authentication.getName())
+      .name("TODO SecurityUtils")
+      .roleId(MOCK_ROLE_ID)
+      .build();
+
+  }
+
+  /**
+   * Returns the LocalAuthority as provided in the security credentials.
+   * @return
+   */
+  public LocalAuthority getCurrentLocalAuthority() {
+    return createMockLocalAuthority();
+  }
+
+
+  // TODO: This should be replaced with a conctrete implemention of something real.
+  private LocalAuthority createMockLocalAuthority() {
+    return LocalAuthority.builder().id(MOCK_LOCAL_AUTHORITY).build();
+  }
+
+  /**
+   * Get the current authentication context.
+   * @return The authentication context
+   * @throws NullPointerException when the authentication context cannot be found.
+   */
+  private Authentication getCurrentAuthenticationContext() {
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+    // Would be a coding error if this called for a non-authenticated area.
+    if (null == authentication) {
+      throw new NullPointerException("No user currently authenticated.");
+    }
+    return authentication;
+
+  }
+}
---
+++ src/main/java/uk/gov/dft/bluebadge/common/security/model/LocalAuthority.java
@@ -0,0 +1,13 @@
+package uk.gov.dft.bluebadge.common.security.model;
+
+import lombok.Builder;
+import lombok.Data;
+
+/**
+ * Provides details of the LocalAuthority provided through the security policy.
+ */
+@Data
+@Builder
+public class LocalAuthority {
+  private int id;
+}
