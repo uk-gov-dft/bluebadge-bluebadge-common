5a8ed20acfc925d142e7c6e4ece59fb9d1f10568
proberts <paul.roberts@valtech.co.uk>
2018-07-25 20:36:46 +0100

    Common classes required by BBB-489 save application

---
+++ src/main/java/uk/gov/dft/bluebadge/common/converter/ToEntityFormatter.java
@@ -0,0 +1,12 @@
+package uk.gov.dft.bluebadge.common.converter;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class ToEntityFormatter {
+
+  public static String postcode(String modelValue) {
+    if (null == modelValue) return null;
+
+    return StringUtils.removeAll(modelValue.toUpperCase(), " ");
+  }
+}
---
+++ src/test/java/uk/gov/dft/bluebadge/common/converter/ToEntityFormatterTest.java
@@ -0,0 +1,14 @@
+package uk.gov.dft.bluebadge.common.converter;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ToEntityFormatterTest {
+  @Test
+  public void postcodeToEntity() {
+
+    assertEquals("WV164AW", ToEntityFormatter.postcode("wv164aw"));
+    assertEquals("WV164AW", ToEntityFormatter.postcode("  wv16 4aw  "));
+  }
+}
