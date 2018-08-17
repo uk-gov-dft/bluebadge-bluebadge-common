e6b417188c29fb01174d3271bd0dbc78ac0cc20f
Stephen Elliott <stephen@bealine.co.uk>
2018-08-17 15:21:07 +0100

    BBB-779 Add test

--- src/test/java/uk/gov/dft/bluebadge/common/security/SecurityUtilsTest.java
+++ src/test/java/uk/gov/dft/bluebadge/common/security/SecurityUtilsTest.java
@@ -40,6 +40,7 @@ public class SecurityUtilsTest {
 
   private SecurityUtils securityUtils;
   private OAuth2Authentication auth2Authentication;
+  private ImmutableMap<String, String> claims;
 
   @Before
   public void setUp() {
@@ -47,17 +48,16 @@ public class SecurityUtilsTest {
     securityUtils = new SecurityUtils();
     SecurityContextHolder.setContext(mockSecurityContext);
     auth2Authentication = new OAuth2Authentication(request, userAuthentication);
+    claims = ImmutableMap.<String, String>builder()
+        .put("local-authority", TEST_LA_SHORT_CODE)
+        .put("client_id", "fakeClient")
+        .put("user_name", DEFAULT_EMAIL_ADDRESS)
+        .build();
   }
 
   private void setupAuthenticationDetails() {
     HttpServletRequest mockHttpRequest = mock(HttpServletRequest.class);
     OAuth2AuthenticationDetails details = new OAuth2AuthenticationDetails(mockHttpRequest);
-    ImmutableMap<String, String> claims =
-        ImmutableMap.<String, String>builder()
-            .put("local-authority", TEST_LA_SHORT_CODE)
-            .put("client_id", "fakeClient")
-            .put("user_name", DEFAULT_EMAIL_ADDRESS)
-            .build();
     details.setDecodedDetails(claims);
     auth2Authentication.setDetails(details);
   }
@@ -68,6 +68,23 @@ public class SecurityUtilsTest {
     // given
     when(mockSecurityContext.getAuthentication()).thenReturn(auth2Authentication);
     setupAuthenticationDetails();
+
+    // when
+    BBPrincipal currentUserDetails = securityUtils.getCurrentAuth();
+
+    // then
+    assertThat(currentUserDetails.getEmailAddress()).isEqualTo(DEFAULT_EMAIL_ADDRESS);
+    assertThat(currentUserDetails.getRoleName()).isEqualTo("ROLE_USER");
+    assertThat(currentUserDetails.getLocalAuthorityShortCode()).isEqualTo(TEST_LA_SHORT_CODE);
+    assertThat(currentUserDetails.getClientId()).isEqualTo("fakeClient");
+  }
+
+  @Test
+  public void shouldReturnAValidPrincipal_whenAuthDetailsIsAMap() {
+
+    // given
+    when(mockSecurityContext.getAuthentication()).thenReturn(auth2Authentication);
+    auth2Authentication.setDetails(claims);
 
     // when
     BBPrincipal currentUserDetails = securityUtils.getCurrentAuth();
